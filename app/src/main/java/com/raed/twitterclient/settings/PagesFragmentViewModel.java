06427bc4fd73879660e5ae4c061d3c2f8441097d
Raed Mughaus <raedradi2014@gmail.com>
2018-09-03 02:46:50 +0400

    Working on the PagesFragment

---
+++ app/src/main/java/com/raed/twitterclient/settings/PagesFragmentViewModel.java
@@ -0,0 +1,20 @@
+package com.raed.twitterclient.settings;
+
+import android.arch.lifecycle.ViewModel;
+
+import com.raed.twitterclient.settings.pages.Page;
+
+import java.util.List;
+
+public class PagesFragmentViewModel extends ViewModel{
+
+
+
+    public List<Page> getPages() {
+
+    }
+
+    public void updatePages(List<Page> mPages) {
+
+    }
+}
